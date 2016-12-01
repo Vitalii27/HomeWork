5a600204646c8ef28a917d72e5157ee9163a78f2
Vitalii Khlystun <v.khlystun01@gmail.com>
2016-12-02 00:05:13 +0200

    HomeWork

---
+++ HomeWork1/JS/main.js
@@ -0,0 +1,17 @@
+//Карточка пользователя
+let name = 'Виталий';
+		lastname = 'Хлыстун'; 
+		datebhd = '29.07.1994';
+		age = 22;
+		skills = ['html', 'css'];
+
+console.log(name, lastname, datebhd, age, skills);
+
+//Карточка друга
+let friend = {
+	name: 'Евгений',
+	age: 22,
+	skills: ['html', 'css', 'php', 'java']
+};
+
+console.log(friend);
