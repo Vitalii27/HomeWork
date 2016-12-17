7c59b83a5b694e96e58dbe90cc3c16edfd6c6bf0
Vitalii Khlystun <v.khlystun01@gmail.com>
2016-12-17 08:31:09 +0200

    homework7

--- HomeWork7/JS/main.js
+++ HomeWork7/JS/main.js
@@ -12,16 +12,16 @@
 
 
 
-    function add(arg1) {
-        function add2(arg2) {
-            function add3(arg3) {
-                return arg1 + arg2 + arg3;
-            }
-            return add3;
+function add(arg1) {
+    function add2(arg2) {
+        function add3(arg3) {
+            return arg1 + arg2 + arg3;
         }
-        return add2;
+        return add3;
     }
-  
+    return add2;
+}
+
 
 console.log(add(1)(2)(3)); // 6
 console.log(add(10)(5)(15)); // 30
@@ -41,17 +41,19 @@ console.log(add(10)(5)(15)); // 30
 function solution3() {
     let counter = 0;
     return {
-        method(){
+
+        method: function () {
             console.log(counter++);
         },
-       Clear(){
-           
-       }
+        clear: function(){
+            
+        }
     }
 }
 
-let call = solution3(); 
+let call = solution3();
 call.method(); //0
 call.method(); //1
 call.method(); //2
-
+call.method()
+call.clear()
